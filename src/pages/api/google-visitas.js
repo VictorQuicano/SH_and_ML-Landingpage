40f482dc47659c0c53e91e590296d026fe96e864
Alejandro Quicaño <alejandroxd25k@gmail.com>
2025-08-10 18:01:03 -0500

    imp: replace private key

--- src/pages/api/google-visitas.js
+++ src/pages/api/google-visitas.js
@@ -5,7 +5,10 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: import.meta.env.FIREBASE_PROJECT_ID,
       clientEmail: import.meta.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: import.meta.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      privateKey: import.meta.env.FIREBASE_PRIVATE_KEY.replace(
+        /\\n/g,
+        "\n"
+      ).replace(/"/g, ""),
     }),
   });
 }
